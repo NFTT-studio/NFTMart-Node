e48eaf817aad1518c4da62b55696b83d36f02fac
chenwei <chenwei767@gmail.com>
2021-06-23 12:28:34 +0800

    Remove flaming network.

--- node/cli/src/command.rs
+++ node/cli/src/command.rs
@@ -54,7 +54,6 @@ impl SubstrateCli for Cli {
 				"" => return Err("Please specify which chain you want to run, e.g. --dev or --chain=local".into()),
 				"dev" => Box::new(chain_spec::development_config()),
 				"local" => Box::new(chain_spec::local_testnet_config()),
-				"fir" | "flaming-fir" => Box::new(chain_spec::flaming_fir_config()?),
 				"staging" => Box::new(chain_spec::staging_testnet_config()),
 				path => Box::new(chain_spec::ChainSpec::from_json_file(
 					std::path::PathBuf::from(path),
