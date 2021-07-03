332ba9e87e37cd64b591b4fa0a0b116178b90d5e
chenwei <chenwei767@gmail.com>
2021-07-03 10:13:40 +0800

    submit_dutch_auction. Part 1.

--- pallets/nftmart-auction/src/lib.rs
+++ pallets/nftmart-auction/src/lib.rs
@@ -7,12 +7,12 @@ use frame_support::{
 };
 use sp_std::vec::Vec;
 use frame_system::pallet_prelude::*;
-pub use nftmart_traits::constants_types::{GlobalId, Balance, ACCURACY, NATIVE_CURRENCY_ID};
+use nftmart_traits::constants_types::{GlobalId, Balance, ACCURACY, NATIVE_CURRENCY_ID};
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
 use sp_runtime::{
-	traits::{AtLeast32BitUnsigned, StaticLookup, Zero, Saturating,},
-	RuntimeDebug, SaturatedConversion, PerU16,
+	traits::{AtLeast32BitUnsigned, StaticLookup, Zero, One, Saturating, CheckedDiv},
+	RuntimeDebug, SaturatedConversion, PerU16, FixedU128, FixedPointNumber,
 };
 use orml_traits::{MultiCurrency, MultiReservableCurrency};
 use nftmart_traits::*;
@@ -78,16 +78,20 @@ pub struct DutchAuction<CurrencyId, BlockNumber, CategoryId, ClassId, TokenId> {
 	#[codec(compact)]
 	pub deposit: Balance,
 	#[codec(compact)]
-	pub start_price: Balance,
+	pub min_price: Balance,
 	#[codec(compact)]
-	pub end_price: Balance,
+	pub max_price: Balance,
 	#[codec(compact)]
 	pub deadline: BlockNumber,
+	#[codec(compact)]
+	pub created_block: BlockNumber,
 	pub items: Vec<OrderItem<ClassId, TokenId>>,
 	pub allow_british_auction: bool,
 	#[codec(compact)]
 	pub min_raise: PerU16,
 }
+
+pub type DutchAuctionBid<AccountId, BlockNumber> = BritishAuctionBid<AccountId, BlockNumber>;
 
 #[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug)]
 enum Releases {
@@ -105,9 +109,11 @@ pub type ClassIdOf<T> = <T as module::Config>::ClassId;
 pub type BalanceOf<T> = <<T as module::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 pub type CurrencyIdOf<T> = <<T as module::Config>::MultiCurrency as MultiCurrency<<T as frame_system::Config>::AccountId>>::CurrencyId;
 pub type BlockNumberOf<T> = <T as frame_system::Config>::BlockNumber;
+pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
 pub type BritishAuctionOf<T> = BritishAuction<CurrencyIdOf<T>,BlockNumberFor<T>,GlobalId,ClassIdOf<T>,TokenIdOf<T>>;
 pub type BritishAuctionBidOf<T> = BritishAuctionBid<AccountIdOf<T>,BlockNumberFor<T>>;
-pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+pub type DutchAuctionOf<T> = DutchAuction<CurrencyIdOf<T>,BlockNumberFor<T>,GlobalId,ClassIdOf<T>,TokenIdOf<T>>;
+pub type DutchAuctionBidOf<T> = DutchAuctionBid<AccountIdOf<T>,BlockNumberFor<T>>;
 
 #[frame_support::pallet]
 pub mod module {
@@ -144,14 +150,19 @@ pub mod module {
 		TooManyTokenChargedRoyalty,
 		InvalidHammerPrice,
 		BritishAuctionNotFound,
+		DutchAuctionNotFound,
 		BritishAuctionBidNotFound,
+		DutchAuctionBidNotFound,
 		BritishAuctionClosed,
+		DutchAuctionClosed,
 		PriceTooLow,
 		CannotRemoveAuction,
 		CannotRedeemAuction,
 		CannotRedeemAuctionNoBid,
 		CannotRedeemAuctionUntilDeadline,
 		DuplicatedBid,
+		MaxPriceShouldBeGreaterThanMinPrice,
+		InvalidDutchMinPrice,
 	}
 
 	#[pallet::event]
@@ -159,6 +170,7 @@ pub mod module {
 	pub enum Event<T: Config> {
 		/// CreatedBritishAuction \[who, auction_id\]
 		CreatedBritishAuction(T::AccountId, GlobalId),
+		CreatedDutchAuction(T::AccountId, GlobalId),
 		/// RemovedBritishAuction \[who, auction_id\]
 		RemovedBritishAuction(T::AccountId, GlobalId),
 		RedeemedBritishAuction(T::AccountId, GlobalId),
@@ -213,42 +225,129 @@ pub mod module {
 	#[pallet::getter(fn british_auction_bids)]
 	pub type BritishAuctionBids<T: Config> = StorageMap<_, Twox64Concat, GlobalId, BritishAuctionBidOf<T>>;
 
+	/// DutchAuctions
+	#[pallet::storage]
+	#[pallet::getter(fn dutch_auctions)]
+	pub type DutchAuctions<T: Config> = StorageDoubleMap<_, Blake2_128Concat, T::AccountId, Twox64Concat, GlobalId, DutchAuctionOf<T>>;
+
+	/// DutchAuctionBids
+	#[pallet::storage]
+	#[pallet::getter(fn dutch_auction_bids)]
+	pub type DutchAuctionBids<T: Config> = StorageMap<_, Twox64Concat, GlobalId, DutchAuctionBidOf<T>>;
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 
-		// #[pallet::weight(100_000)]
-		// #[transactional]
-		// #[allow(clippy::too_many_arguments)]
-		// pub fn submit_dutch_auction(
-		// 	origin: OriginFor<T>,
-		// 	#[pallet::compact] currency_id: CurrencyIdOf<T>,
-		// 	#[pallet::compact] category_id: GlobalId,
-		// 	#[pallet::compact] deposit: Balance,
-		// 	#[pallet::compact] start_price: Balance,
-		// 	#[pallet::compact] end_price: Balance,
-		// 	#[pallet::compact] deadline: BlockNumberOf<T>,
-		// 	items: Vec<(ClassIdOf<T>, TokenIdOf<T>, TokenIdOf<T>)>,
-		// 	allow_british_auction: bool,
-		// 	#[pallet::compact] min_raise: PerU16,
-		// ) -> DispatchResultWithPostInfo {
-		//
-		//
-		//
-		// 	Ok(().into())
-		// }
-		//
-		// #[pallet::weight(100_000)]
-		// #[transactional]
-		// pub fn bid_dutch_auction(
-		// 	origin: OriginFor<T>,
-		// 	#[pallet::compact] price: Balance,
-		// 	auction_owner: <T::Lookup as StaticLookup>::Source,
-		// 	#[pallet::compact] auction_id: GlobalId,
-		// ) -> DispatchResultWithPostInfo {
-		//
-		//
-		// 	Ok(().into())
-		// }
+		#[pallet::weight(100_000)]
+		#[transactional]
+		#[allow(clippy::too_many_arguments)]
+		pub fn submit_dutch_auction(
+			origin: OriginFor<T>,
+			#[pallet::compact] currency_id: CurrencyIdOf<T>,
+			#[pallet::compact] category_id: GlobalId,
+			#[pallet::compact] deposit: Balance,
+			#[pallet::compact] min_price: Balance,
+			#[pallet::compact] max_price: Balance,
+			#[pallet::compact] deadline: BlockNumberOf<T>,
+			items: Vec<(ClassIdOf<T>, TokenIdOf<T>, TokenIdOf<T>)>,
+			allow_british_auction: bool,
+			#[pallet::compact] min_raise: PerU16,
+		) -> DispatchResultWithPostInfo {
+			let who: T::AccountId = ensure_signed(origin)?;
+
+			// check and reserve `deposit`
+			ensure!(deposit >= T::ExtraConfig::get_min_order_deposit(), Error::<T>::SubmitWithInvalidDeposit);
+			<T as Config>::Currency::reserve(&who, deposit.saturated_into())?;
+
+			let created_block = frame_system::Pallet::<T>::block_number();
+			// check deadline
+			ensure!(created_block < deadline, Error::<T>::SubmitWithInvalidDeadline);
+
+			// check min price and max price
+			ensure!(0 < min_price , Error::<T>::InvalidDutchMinPrice);
+			ensure!(min_price < max_price , Error::<T>::MaxPriceShouldBeGreaterThanMinPrice);
+
+			let mut auction: DutchAuctionOf<T> = DutchAuction {
+				currency_id,
+				deposit,
+				min_price,
+				max_price,
+				deadline,
+				created_block,
+				category_id,
+				items: Vec::with_capacity(items.len()),
+				allow_british_auction,
+				min_raise,
+			};
+
+			ensure!(
+				count_charged_royalty::<T::AccountId, ClassIdOf<T>, TokenIdOf<T>, T::NFT>(&items)? <= 1,
+				Error::<T>::TooManyTokenChargedRoyalty,
+			);
+
+			for (class_id, token_id, quantity) in items{
+				T::NFT::reserve_tokens(&who, class_id, token_id, quantity)?;
+				auction.items.push(OrderItem{
+					class_id,
+					token_id,
+					quantity,
+				})
+			}
+
+			// add the auction to a category
+			T::ExtraConfig::inc_count_in_category(category_id)?;
+
+			// generate an auction id
+			let auction_id = T::ExtraConfig::get_then_inc_id()?;
+
+			// save auction information.
+			DutchAuctions::<T>::insert(&who, auction_id, auction);
+
+			let auction_bid: DutchAuctionBidOf<T> = DutchAuctionBid {
+				last_offer_price: min_price,
+				last_offer_account: None,
+				last_offer_block: Zero::zero(),
+			};
+
+			BritishAuctionBids::<T>::insert(auction_id, auction_bid);
+
+			// emit event.
+			Self::deposit_event(Event::CreatedDutchAuction(who, auction_id));
+			Ok(().into())
+		}
+
+		#[pallet::weight(100_000)]
+		#[transactional]
+		pub fn bid_dutch_auction(
+			origin: OriginFor<T>,
+			#[pallet::compact] price: Balance,
+			auction_owner: <T::Lookup as StaticLookup>::Source,
+			#[pallet::compact] auction_id: GlobalId,
+		) -> DispatchResultWithPostInfo {
+			let purchaser: T::AccountId = ensure_signed(origin)?;
+			let auction_owner: T::AccountId = T::Lookup::lookup(auction_owner)?;
+			let auction: DutchAuctionOf<T> = Self::dutch_auctions(&auction_owner, auction_id).ok_or(Error::<T>::DutchAuctionNotFound)?;
+			let auction_bid: DutchAuctionBidOf<T> = Self::dutch_auction_bids(auction_id).ok_or(Error::<T>::DutchAuctionBidNotFound)?;
+
+			match (auction_bid.last_offer_account, auction.allow_british_auction) {
+				(None, false) => {
+					// check deadline
+					let current_block: BlockNumberOf<T> = frame_system::Pallet::<T>::block_number();
+					ensure!(auction.deadline >= current_block, Error::<T>::DutchAuctionClosed);
+					let current_price: Balance = calc_current_price::<T>(&auction, current_block);
+
+				},
+				(last_offer_account, true) => {
+					if let Some(last_offer_account) = last_offer_account {
+
+					}
+				},
+				_ => {
+					return Err(Error::<T>::DutchAuctionClosed.into());
+				}
+			}
+			Ok(().into())
+		}
 
 		/// Create an British auction.
 		///
@@ -466,4 +565,32 @@ impl<T: Config> Pallet<T> {
 			auction.deadline
 		}
 	}
+}
+
+fn calc_current_price<T: Config>(auction: &DutchAuctionOf<T>, current_block: BlockNumberOf<T>) -> Balance {
+	let created_block: BlockNumber = auction.created_block.saturated_into();
+	let aligned_block: BlockNumber = current_block
+		.saturated_into::<BlockNumber>()
+		.saturating_sub(created_block) // >= 0
+		.checked_div(time::MINUTES * 30) // >= 0
+		.map(|x| x.saturating_mul(time::MINUTES * 30)) // >= 0
+		.map(|x| x.saturating_add(created_block)) // >= created_block
+		.unwrap_or(created_block); // >= created_block
+
+	let deadline: FixedU128 = (auction.deadline.saturated_into::<BlockNumber>(), 1).into();
+	let created_block: FixedU128 = (created_block, 1).into();
+	let current_block: FixedU128 = (aligned_block, 1).into();
+	let max_price: FixedU128 = (auction.max_price, ACCURACY).into();
+	let min_price: FixedU128 = (auction.min_price, ACCURACY).into();
+
+	// calculate current price.
+	let current_price: Balance = max_price
+		.saturating_sub(min_price) // > 0
+		.saturating_mul(current_block.saturating_sub(created_block)) // >= 0
+		.checked_div(&deadline.saturating_sub(created_block)) // >= 0
+		.map(|x| max_price.saturating_sub(x)) // >= min_price && <= max_price
+		.unwrap_or(max_price) // >= min_price && <= max_price
+		.saturating_mul_int(ACCURACY); // >= min_price && <= max_price
+
+	current_price
 }
--- pallets/nftmart-auction/src/tests.rs
+++ pallets/nftmart-auction/src/tests.rs
@@ -4,10 +4,12 @@
 
 use super::{NATIVE_CURRENCY_ID};
 use crate::mock::*;
-use sp_runtime::PerU16;
+use sp_runtime::{PerU16, SaturatedConversion};
 use orml_nft::AccountToken;
+use nftmart_traits::*;
 use frame_support::{assert_ok};
 use paste::paste;
+use crate::{DutchAuction, DutchAuctionOf};
 
 macro_rules! submit_british_auction_should_work {
     ( $(#[$attr: meta])* $test_name: ident, $hammer_price: expr) => {
@@ -257,5 +259,111 @@ fn remove_british_auction_should_work() {
 		assert_eq!(last_event(), event);
 		assert!(get_bid(auction_id).is_none());
 		assert!(get_auction(&BOB, auction_id).is_none());
+	});
+}
+
+#[test]
+fn calc_current_price_should_work() {
+	let auction: DutchAuctionOf<Runtime> = DutchAuction {
+		currency_id: NATIVE_CURRENCY_ID,
+		category_id: 1,
+		deposit: 10 * ACCURACY,
+		min_price: 100 * ACCURACY,
+		max_price: 10000 * ACCURACY,
+		deadline: (time::MINUTES * 120).saturated_into(),
+		created_block: 0,
+		items: vec![],
+		allow_british_auction: false,
+		min_raise: Default::default(),
+	};
+	for (x, y) in vec![
+		(0, 10000), (1, 10000), (29, 10000),
+		(30, 7525), (31, 7525), (59, 7525),
+		(60, 5050), (61, 5050), (89, 5050),
+		(90, 2575), (91, 2575), (119, 2575),
+		(120, 100), (121, 100), (149, 100),
+		(150, 0), (151, 0),  (1511, 0), (15111, 0),
+	] {
+		assert_eq!(
+			crate::calc_current_price::<Runtime>(&auction, (time::MINUTES * x).saturated_into()),
+			y * ACCURACY,
+			"x={}, y={}", x, y,
+		);
+	}
+	let auction: DutchAuctionOf<Runtime> = DutchAuction {
+		currency_id: NATIVE_CURRENCY_ID,
+		category_id: 1,
+		deposit: 10 * ACCURACY,
+		min_price: 100 * ACCURACY,
+		max_price: 10000 * ACCURACY,
+		deadline: 1,
+		created_block: 0,
+		items: vec![],
+		allow_british_auction: false,
+		min_raise: Default::default(),
+	};
+	for (x, y) in vec![
+		(0, 10000), (1, 10000), (2, 10000), (29*time::MINUTES, 10000),
+		(29*time::MINUTES + 9, 10000), (29*time::MINUTES + 10, 0),
+	] {
+		assert_eq!(
+			crate::calc_current_price::<Runtime>(&auction, x.saturated_into()),
+			y * ACCURACY,
+			"x={}, y={}", x, y,
+		);
+	}
+	let auction: DutchAuctionOf<Runtime> = DutchAuction {
+		currency_id: NATIVE_CURRENCY_ID,
+		category_id: 1,
+		deposit: 10 * ACCURACY,
+		min_price: 100,
+		max_price: 101,
+		deadline: 1,
+		created_block: 0,
+		items: vec![],
+		allow_british_auction: false,
+		min_raise: Default::default(),
+	};
+	for (x, y) in vec![
+		(0, 101), (1, 101), (2, 101), (29*time::MINUTES, 101),
+		(29*time::MINUTES + 9, 101), (29*time::MINUTES + 10, 0),
+	] {
+		assert_eq!(
+			crate::calc_current_price::<Runtime>(&auction, x.saturated_into()),
+			y,
+			"x={}, y={}", x, y,
+		);
+	}
+}
+
+#[test]
+fn submit_dutch_auction_should_work() {
+	ExtBuilder::default().build().execute_with(|| {
+		add_class(ALICE);
+		add_token(BOB, 20, None);
+		add_token(BOB, 40, Some(false));
+		let cate_id = current_gid();
+		add_category();
+		let auction_id = current_gid();
+
+		assert_ok!(NftmartAuction::submit_dutch_auction(
+			Origin::signed(BOB),
+			NATIVE_CURRENCY_ID,
+			cate_id,
+			50, // deposit
+			200, // min_price
+			500, // max_price
+			10, // deadline
+			vec![(CLASS_ID0, TOKEN_ID0, 10), (CLASS_ID0, TOKEN_ID1, 20)],
+			false,
+			PerU16::from_percent(50),
+		));
+		let event = Event::NftmartAuction(crate::Event::CreatedDutchAuction(BOB, auction_id));
+		assert_eq!(last_event(), event);
+
+		assert_eq!(vec![
+			(CLASS_ID0, TOKEN_ID0, AccountToken { quantity: 10, reserved: 10 }),
+			(CLASS_ID0, TOKEN_ID1, AccountToken { quantity: 20, reserved: 20 })
+		], all_tokens_by(BOB));
 	});
 }
