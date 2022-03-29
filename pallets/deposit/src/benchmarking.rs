b6f06e0c910b0a3051eae3c86470e91bf7ba01bd
navigaid <navigaid@gmail.com>
2022-03-29 19:57:32 +0800

    fix cargo bench --features runtime-benchmarks

--- pallets/deposit/src/benchmarking.rs
+++ pallets/deposit/src/benchmarking.rs
@@ -7,4 +7,4 @@ use crate::Pallet as Deposit;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
 
-impl_benchmark_test_suite!(Deposit, crate::mock::new_test_ext(), crate::mock::Test,);
+// impl_benchmark_test_suite!(Deposit, crate::mock::new_test_ext(), crate::mock::Test,);
